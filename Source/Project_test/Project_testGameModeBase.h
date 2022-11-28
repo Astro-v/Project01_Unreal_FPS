06010b78b4e5fd15334b3edbcb0164e86a096e3a
astro-v <valentinmichel07200@gmail.com>
2022-11-28 18:36:23 +0100

    Firt Commit
    Importing the project and first commit

---
+++ Source/Project_test/Project_testGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_testGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_TEST_API AProject_testGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
